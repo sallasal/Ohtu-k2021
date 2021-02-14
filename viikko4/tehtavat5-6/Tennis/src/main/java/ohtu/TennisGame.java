ef6ce9efa9c6861640fbe13c01b2be8c0133992e
sallasal <47078225+sallasal@users.noreply.github.com>
2021-02-14 20:14:46 +0200

    Add exercise 6 (triple-programmed)

--- viikko4/tehtavat5-6/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/tehtavat5-6/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,11 @@
+//Proudly triple-programmed with Mary Ehrstedt, Jenny Perttola and Salla Salokanto
+
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,68 +15,66 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            score = evenGame(player1Score);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            score = winOrAdvantage(player1Score - player2Score);
+        } else {
+            score = tennisPointsDescription(player1Score);
+            score += "-";
+            score += tennisPointsDescription(player2Score);
         }
         return score;
+    }
+
+    private String evenGame(int score) {
+        switch (score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private String winOrAdvantage(int scoreDifference) {
+        if (scoreDifference == 1) {
+            return "Advantage player1";
+        } else if (scoreDifference == -1) {
+            return "Advantage player2";
+        } else if (scoreDifference >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+    
+        private String tennisPointsDescription(int points) {
+        switch (points) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return "";
     }
 }
