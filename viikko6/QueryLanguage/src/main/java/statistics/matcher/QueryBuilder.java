f7e171784b72850cc790b783926fd108e925ef86
sallasal <47078225+sallasal@users.noreply.github.com>
2021-03-02 18:34:02 +0200

    Add exercise 4

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,31 @@
+
+package statistics.matcher;
+
+import statistics.*;
+
+public class QueryBuilder {
+    private Matcher matcher;
+    
+    public QueryBuilder() {
+        this.matcher = new All();
+    }
+    
+    public Matcher build() {
+        return matcher;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(new PlaysIn(team), matcher);
+        return this;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(new HasAtLeast(value, category), matcher);
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(new HasFewerThan(value, category), matcher);
+        return this;
+    }
+}
